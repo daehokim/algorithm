d9debb8961afcfb862b6706bcc491c966f26ba58
nadaeho <42531973+daehokim@users.noreply.github.com>
2019-08-13 14:20:19 +0900

    Create 2798_블랙잭.java

---
+++ baekjoon/2798_블랙잭.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int n, m, card[], ans, tmp;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		card = new int[n];
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < n; ++i) card[i] = Integer.parseInt(st.nextToken());
+		
+		ans = 0;
+		for(int i = 0; i < n-2; ++i) {
+			for(int j = i+1; j < n-1; ++j) {
+				for(int k = j+1; k < n; ++k) {
+					tmp = card[i]+card[j]+card[k];
+					if(tmp <= m && tmp > ans) ans = tmp;
+				}
+			}
+		}
+		
+		System.out.println(ans);
+	}
+}
