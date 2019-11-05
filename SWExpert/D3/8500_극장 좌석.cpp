e175ccbb435269218838064acd159f7a2c3cc9f5
nadaeho <42531973+daehokim@users.noreply.github.com>
2019-11-06 08:52:59 +0900

    Create 8500_극장 좌석.cpp

---
+++ SWExpert/D3/8500_극장 좌석.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T = 0, n = 0, ans = 0, max = 0;
+	int* A = NULL;
+
+	cin >> T;
+	for(int tc = 1; tc <= T; ++tc) {
+		cin >> n;
+		A = new int[n];
+		ans = n;
+		max = 0;
+		for(int i = 0; i < n; ++i) {
+			cin >> A[i];
+			ans += A[i];
+			if(A[i] > max) {
+				max = A[i];
+			}
+		}
+		cout << "#" << tc << " " << ans+max << endl;
+	}
+	return 0;
+}
