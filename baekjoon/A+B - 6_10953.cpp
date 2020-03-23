18988a8c7eae6a22f9d33e2201ac537319a38b4d
nadaeho <42531973+daehokim@users.noreply.github.com>
2020-03-23 22:13:33 +0900

    Create A+B - 6_10953.cpp

---
+++ baekjoon/A+B - 6_10953.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int T;
+char arr[4] = { 0, };
+
+int main(void)
+{
+	scanf("%d", &T);
+	for (int tc = 0; tc < T; ++tc) {
+		scanf("%s", &arr);
+		int ans = 
+		printf("%d\n", arr[0]+arr[2]-2*'0');
+	}
+	return 0;
+}
